cb117148337b7431b71475418f6c207f7419a47a
kirillparaschenko <kparaschenko@caeonline.com>
2023-02-22 18:23:13 +0300

    Added branch HomeTask2, Changed Main, Added Lesson1_Task1

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-string name = Console.ReadLine();
-Console.WriteLine("Hello," + name + "!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lesson1_Task1();
+        }
+        public static void Lesson1_Task1() 
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello," + name + "!");
+        }
+    }
+}
+
