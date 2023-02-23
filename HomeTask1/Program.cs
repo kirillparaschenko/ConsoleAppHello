30622242222985f4900e966b276dea74ef258c84
kirillparaschenko <kparaschenko@caeonline.com>
2023-02-23 12:14:28 +0300

    Added Lesson2_Task2_RangeDetermination

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -9,19 +9,20 @@ namespace MyApp
         {
             //Lesson1_Task1();
             Lesson2_Task1_Calculator();
+            Lesson2_Task2_RangeDetermination();
         }
         public static void Lesson1_Task1()
         {
-            string name = Console.ReadLine();
+            string? name = Console.ReadLine();
             Console.WriteLine("Hello," + name + "!");
         }
         public static void Lesson2_Task1_Calculator()
         {
-            double operand1 = 10;
-            double operand2 = 6;
+            double operand1 = 10.123;
+            double operand2 = 6.789;
             double result = 0;
-            Console.Write("Enter sign: ");
-            string sign = Console.ReadLine();
+            Console.Write("Enter sign (+,-,*,/): ");
+            string? sign = Console.ReadLine();
             switch (sign)
             {
                 case "+":
@@ -46,6 +47,32 @@ namespace MyApp
                     break;
             }
             Console.WriteLine($"{operand1} {sign} {operand2} = {result}");
+        }
+        /*Напишите программу определения, попадает ли указанное пользователем число от 0 до 100 в числовой промежуток[0 - 14][15 - 35][36 - 49][50 - 100].
+Если да, то укажите, в какой именно промежуток.
+Если пользователь указывает число, не входящее ни в один из имеющихся числовых промежутков, то выводится соответствующее сообщение.*/
+        public static void Lesson2_Task2_RangeDetermination()
+        {
+            Console.Write("Enter number: ");
+            string? number = Console.ReadLine();
+            double x = Convert.ToDouble(number);
+            if (x >= 0 && x <= 14)
+            {
+                Console.WriteLine($"{number} in Range [0 - 14]");
+            }
+            else if (x >= 15 && x <= 35)
+            {
+                Console.WriteLine($"{number} in Range [15 - 35]");
+            }
+            else if (x >= 36 && x <= 49)
+            {
+                Console.WriteLine($"{number} in Range [36 - 49]");
+            }
+            else if (x >= 50 && x <= 100) 
+            {
+                Console.WriteLine($"{number} in Range [50 - 100]");
+            }
+            else { Console.WriteLine($"{number} out of Range [0 - 100]"); }
         }
     }
 }
