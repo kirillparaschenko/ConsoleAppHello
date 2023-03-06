000071949a27e08190e0cd214314d6bceb28746b
kirillparaschenko <kparaschenko@caeonline.com>
2023-03-06 10:46:07 +0300

    Clean Main

--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -8,23 +8,7 @@ namespace ConsoleAppHello.HomeTasks
     {
         static void Main(string[] args)
         {
-            //HomeWork1.Task1();
-            //HomeWork2.Task1_Calculator();
-            //HomeWork2.Task2_RangeDetermination();
-            //HomeWork2.Task3_RangeDetermination();
-            //HomeWork2.Task4_ParityCheck();
-            //HomeWork2.Task4_1_ParityCheck();
-            //HomeWork3.Task0();
-            //HomeWork3.Task1();
-            //HomeWork3.Task2();
-            //HomeWork3.Task3();
-            //HomeWork4.Task1();
-            //HomeWork4.Task2();
-            //HomeWork4.Task3();
-            //HomeWork4.Task4();
-            HomeWork4Task5.Task5_1();
-            HomeWork4Task5.Task5_2();
-            HomeWork4Task5.Task5_3();
+
         }
     }
 }
