6d378844a6fdbc66276915a90512480ef777e538
kirillparaschenko <kparaschenko@caeonline.com>
2023-03-06 11:05:40 +0300

    Added branch HomeWork2, Added class Phone, comleted task 1

---
+++ HomeTasks/HomeWork5.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHello.HomeTasks
+{
+    /// <summary>
+    /// 1. Класс Phone.  
+    /// Создайте класс Phone, который содержит переменные number, model и weight. Создайте три экземпляра этого класса.  
+    /// Выведите на консоль значения их переменных.  
+    /// Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего. Выводит на консоль сообщение “Звонит { name}”. 
+    /// getNumber –  возвращает номер телефона.Вызвать эти методы для каждого из  объектов.
+    /// Добавить конструктор в класс Phone, который принимает на вход три параметра для инициализации переменных класса - number, model и  weight.
+    /// Добавить конструктор, который принимает на вход два параметра для  инициализации переменных класса - number, model.
+    /// Добавить конструктор без параметров.
+    /// Вызвать из конструктора с тремя параметрами конструктор с двумя.  
+    /// Добавьте перегруженный метод receiveCall, который принимает два параметра - имя звонящего и номер телефона звонящего. Вызвать этот  метод.
+    /// Создать метод sendMessage с аргументами переменной длины.Данный метод принимает на вход номера телефонов, которым будет отправлено сообщение. 
+    /// Метод выводит на консоль номера этих телефонов.
+    /// </summary>
+    internal class Phone
+    {
+        public int number;
+        public string model;
+        public double weight;
+
+        public void ReceiveCall(string name)
+        {
+            Console.WriteLine($"Звонит {name}");
+        }
+
+        public void ReceiveCall(string name, int number)
+        {
+            Console.WriteLine($"Звонит {name} {number}");
+        }
+
+        public int GetNumber(int number)
+        {
+            return number;
+            Console.WriteLine($"Number: {number}");
+        }
+
+        public static void SendMessages(params int[] list)
+        {
+            foreach (int i in list)
+            {
+                Console.WriteLine($"Number: {i}");
+            }
+        }
+
+        public Phone(int number, string model, double weight)
+        {
+            this.number = number;
+            this.model = model;
+            this.weight = weight;
+        }
+
+        public Phone(int number, string model) : this(number, model, 0)
+        {
+            this.number = number;
+            this.model = model;
+        }
+
+        public Phone()
+        {
+        }
+    }
+}
--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -8,7 +8,28 @@ namespace ConsoleAppHello.HomeTasks
     {
         static void Main(string[] args)
         {
+            //Task 1
 
+            Phone phone1 = new Phone(291111111, "Samsung", 111);
+            Phone phone2 = new Phone(292222222, "Nokia", 112);
+            Phone phone3 = new Phone(293333333, "Apple", 113);
+
+            Console.WriteLine($"Number: {phone1.number}, Model: {phone1.model}, Weight: {phone1.weight}");
+            Console.WriteLine($"Number: {phone2.number}, Model: {phone2.model}, Weight: {phone2.weight}");
+            Console.WriteLine($"Number: {phone3.number}, Model: {phone3.model}, Weight: {phone3.weight}");
+
+            phone1.GetNumber(phone1.number);
+            phone2.GetNumber(phone2.number);
+            phone3.GetNumber(phone3.number);
+
+            phone1.ReceiveCall("Olya");
+            phone2.ReceiveCall("Liza");
+            phone3.ReceiveCall("Vasya");
+
+            phone1.ReceiveCall("Andrei", phone1.number);
+
+            int[] list = { phone1.number, phone2.number, phone3.number };
+            Phone.SendMessages(list);
         }
     }
 }
