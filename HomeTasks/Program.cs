895cfc60eb0e781517da34fa7bc157faf3997b57
kirillparaschenko <kparaschenko@caeonline.com>
2023-03-10 14:34:23 +0300

    Added classes Figure, Triangles, Rectangles, completed Task1

--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ConsoleAppHello.HomeTasks.HomeWork6;
+using ConsoleAppHello.HomeTasks.HomeWork6.Rectangles;
+using ConsoleAppHello.HomeTasks.HomeWork6.Triangles;
+using System;
 using System.Transactions;
 using System.Xml.Linq;
 
@@ -8,7 +11,21 @@ namespace ConsoleAppHello.HomeTasks
     {
         static void Main(string[] args)
         {
+            //Task1 создавь массив квадратов, прямоугольник и треугольников и вывести их площади
 
+            Figure triangle1 = new TriangleBuilder().CreateTriangle(7, 6, 8);
+            Figure triangle2 = new TriangleBuilder().CreateTriangle(3, 3, 3);
+            Figure triangle3 = new TriangleBuilder().CreateTriangle(4, 4, 2);
+            Figure triangle4 = new TriangleBuilder().CreateTriangle(3, 4, 5);
+            Figure rectangle1 = new Rectangle(5, 6);
+            Figure square1 = new Square(6, 6);
+
+            Figure[] figures = new Figure[] { triangle1, triangle2, triangle3, triangle4, rectangle1, square1 };
+            foreach (var obj in figures) 
+            {
+                double square = obj.GetSquare();
+                Console.WriteLine(square);
+            }
         }
     }
 }
