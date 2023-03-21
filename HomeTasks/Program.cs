70c7dcf0b880cd10af15e0b7ff16ceacbf0971f6
kirillparaschenko <kparaschenko@caeonline.com>
2023-03-21 14:23:10 +0300

    Added branch HomeTask8, Completed task 1, 2

--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Transactions;
 using System.Xml.Linq;
 
@@ -8,7 +9,37 @@ namespace ConsoleAppHello.HomeTasks
     {
         static void Main(string[] args)
         {
+            //1. Необходимо перехватить ошибку и вывести на экран с указанием типа этой ошибки. 
+            ArrayList list = new ArrayList();
+            try
+            {
+                object s = list[18];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
+            //2. Необходимо создать коллекцию Dictionary, в которой будет находиться 10 различных пар объектов.
+            //Необходимо вывести содержимое коллекции на экран.
+
+            var dictonary = new Dictionary<string, string>();
+
+            dictonary.Add("холодно", "cold");
+            dictonary.Add("тепло", "warm");
+            dictonary.Add("жарко" , "hot");
+            dictonary.Add("морозно" , "frosty");
+            dictonary.Add("солнечно" , "sunny");
+            dictonary.Add("ясно" , "clear");
+            dictonary.Add("душно" , "stuffy");
+            dictonary.Add("облачно" , "cloudy");
+            dictonary.Add("ветренно" , "windy");
+            dictonary.Add("сухо" , "dry");
+
+            foreach (var item in dictonary) 
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
         }
     }
 }
